0566138cce0613d06d45f12e2361db9d7aeefcc2
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-12-22 10:20:24 +0100

    Fix VERCEL_URL

--- src/env.js
+++ src/env.js
@@ -19,7 +19,7 @@ export const env = createEnv({
       // Since NextAuth.js automatically uses the VERCEL_URL if present.
       (str) => process.env.VERCEL_URL ?? str,
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url(),
+      process.env.VERCEL_URL ? z.string() : z.string().url(),
     ),
 
     POSTGRES_URL: z.string().url(),
