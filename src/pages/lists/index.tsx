f86e851dfda35ea2371315a8187b54fb7859eb17
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-01-18 08:50:16 +0100

    Add New list button

--- src/pages/lists/index.tsx
+++ src/pages/lists/index.tsx
@@ -1,10 +1,17 @@
 import { Layout } from "~/layouts/DefaultLayout";
 
 import { Lists } from "~/features/lists/components/Lists";
+import { Button } from "~/components/ui/Button";
+import Link from "next/link";
 
 export default function ListsPage() {
   return (
     <Layout sidebar="left" showBallot eligibilityCheck>
+      <div className="flex justify-end">
+        <Button as={Link} href={`/lists/new`} variant="primary">
+          New list
+        </Button>
+      </div>
       <Lists />
     </Layout>
   );
