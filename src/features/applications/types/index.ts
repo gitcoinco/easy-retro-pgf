4bc880b11efce211e5662f03cdeaef4a3e2696fc
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-01-26 14:17:04 +0100

    At least one impact category must be selected

--- src/features/applications/types/index.ts
+++ src/features/applications/types/index.ts
@@ -35,7 +35,7 @@ export const ApplicationSchema = z.object({
   payoutAddress: z.string().startsWith("0x"),
   contributionDescription: z.string().min(3),
   impactDescription: z.string().min(3),
-  impactCategory: z.array(z.string()),
+  impactCategory: z.array(z.string()).min(1),
   contributionLinks: z
     .array(
       z.object({
