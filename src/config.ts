c257fada57c931a9a8349ee36a2515582d6b6d6e
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-02-02 06:16:24 +0100

    Remove unused schemas in config

--- src/config.ts
+++ src/config.ts
@@ -48,15 +48,6 @@ export const eas = {
   schemas: {
     metadata: process.env.NEXT_PUBLIC_METADATA_SCHEMA!,
     approval: process.env.NEXT_PUBLIC_APPROVAL_SCHEMA!,
-
-    // TODO: remove these
-    applicationsSchema: process.env.NEXT_PUBLIC_APPLICATIONS_SCHEMA ?? "",
-    approvedApplicationsSchema:
-      process.env.NEXT_PUBLIC_APPROVED_APPLICATIONS_SCHEMA ?? "",
-    badgeholderAttester: process.env.NEXT_PUBLIC_BADGEHOLDER_ATTESTER ?? "",
-    badgeholder: process.env.NEXT_PUBLIC_BADGEHOLDER_SCHEMA ?? "",
-    profileSchema: process.env.NEXT_PUBLIC_PROFILE_SCHEMA ?? "",
-    listsSchema: process.env.NEXT_PUBLIC_LISTS_SCHEMA ?? "",
   },
 };
 
