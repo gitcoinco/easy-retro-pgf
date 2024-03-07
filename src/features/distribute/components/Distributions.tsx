dbf52992fc7f12ac465c4303f1de718c8882ef75
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-03-07 15:28:03 +0100

    Add export csv

--- src/features/distribute/components/Distributions.tsx
+++ src/features/distribute/components/Distributions.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { FileDown } from "lucide-react";
+import { useCallback, useState } from "react";
 import { z } from "zod";
 import { EmptyState } from "~/components/EmptyState";
 import { Button, IconButton } from "~/components/ui/Button";
@@ -12,7 +13,9 @@ import {
   type Distribution,
   DistributionSchema,
 } from "~/features/distribute/types";
+import { useProjectsById } from "~/features/projects/hooks/useProjects";
 import { api } from "~/utils/api";
+import { format } from "~/utils/csv";
 
 export function Distributions() {
   const [confirmDistribution, setConfirmDistribution] = useState<
@@ -69,9 +72,12 @@ export function Distributions() {
         <div className="mb-2 flex items-center justify-between gap-2">
           <h1 className="text-3xl font-bold">Distribute</h1>
 
-          <Button variant="primary" type="submit">
-            Distribute tokens
-          </Button>
+          <div className="flex gap-2">
+            <ExportCSV votes={distributions} />
+            <Button variant="primary" type="submit">
+              Distribute tokens
+            </Button>
+          </div>
         </div>
 
         <div className="min-h-[360px] overflow-auto">
@@ -131,5 +137,35 @@ function ConfirmDistributionDialog({
         </Button>
       </div>
     </Dialog>
+  );
+}
+
+function ExportCSV({ votes }: { votes: Distribution[] }) {
+  // Fetch projects for votes to get the name
+  const projects = useProjectsById(votes.map((v) => v.projectId));
+
+  const exportCSV = useCallback(async () => {
+    // Append project name to votes
+    const votesWithProjects = votes.map((vote) => ({
+      ...vote,
+      name: projects.data?.find((p) => p.id === vote.projectId)?.name,
+    }));
+
+    // Generate CSV file
+    const csv = format(votesWithProjects, {
+      columns: ["projectId", "name", "payoutAddress", "amount"],
+    });
+    window.open(`data:text/csv;charset=utf-8,${csv}`);
+  }, [votes]);
+
+  return (
+    <IconButton
+      type="button"
+      icon={FileDown}
+      disabled={projects.isLoading}
+      onClick={exportCSV}
+    >
+      Export CSV
+    </IconButton>
   );
 }
