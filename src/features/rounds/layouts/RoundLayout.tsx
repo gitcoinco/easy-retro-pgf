95fdb0dda008f0735f81b65b509064851b6b2301
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-03-26 18:01:40 +0100

    Remove unused file

--- src/features/rounds/layouts/RoundLayout.tsx
+++
@@ -1,7 +0,0 @@
-import { type PropsWithChildren } from "react";
-import { BaseLayout } from "~/layouts/BaseLayout";
-import { Layout } from "~/layouts/DefaultLayout";
-
-export function RoundLayout(props: PropsWithChildren) {
-  return <Layout {...props} />;
-}
