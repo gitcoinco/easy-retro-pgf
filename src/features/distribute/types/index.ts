cca51268582eb5404b61f945f3ad751d3496578b
0xmad <0xmad@users.noreply.github.com>
2024-04-02 09:16:44 -0500

    fix: voters validation

--- src/features/distribute/types/index.ts
+++ src/features/distribute/types/index.ts
@@ -2,7 +2,12 @@ import { type Address, isAddress } from "viem";
 import { z } from "zod";
 
 export const EthAddressSchema = z.custom<string>(
-  (val) => isAddress(val as Address),
+  (val) =>
+    (val as string)
+      .split(",")
+      .map((address) => address.trim())
+      .every((address) => isAddress(address as Address)) ||
+    isAddress(val as Address),
   "Invalid address",
 );
 
