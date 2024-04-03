a5577f2e69aa47f0aed13bf518cab28df8394a53
0xmad <0xmad@users.noreply.github.com>
2024-04-03 09:34:49 -0500

    fix: add start block for maci signup event parsing

--- src/config.ts
+++ src/config.ts
@@ -28,6 +28,7 @@ export const config = {
   network:
     wagmiChains[process.env.NEXT_PUBLIC_CHAIN_NAME as keyof typeof wagmiChains],
   maciAddress: process.env.NEXT_PUBLIC_MACI_ADDRESS,
+  maciStartBlock: Number(process.env.NEXT_PUBLIC_MACI_START_BLOCK ?? 0),
   tallyUrl: process.env.NEXT_PUBLIC_TALLY_URL,
 };
 
