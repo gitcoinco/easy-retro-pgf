d9634d0336fd802c8baee68c3b847c955cbb9034
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-04-06 08:41:55 +0200

    Add landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
+import { Plus } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "~/components/ui/Button";
+import { RoundProgress } from "~/features/info/components/RoundProgress";
 import { Layout } from "~/layouts/DefaultLayout";
 
-import { type GetServerSideProps } from "next";
-
-export default function ProjectsPage({}) {
-  return <Layout>...</Layout>;
+export default function LandingPage({}) {
+  return (
+    <Layout>
+      <div className="flex items-center justify-between">
+        <h1 className="text-4xl font-bold">Filecoin RetroPGF</h1>
+        <Button icon={Plus} as={Link} href={"/applications/new"}>
+          Apply with your project
+        </Button>
+      </div>
+      <RoundProgress />
+      <Image
+        alt="Filecoin RetroPGF"
+        src={"/filrpgf.jpg"}
+        width={1600}
+        height={900}
+      />
+    </Layout>
+  );
 }
-
-export const getServerSideProps: GetServerSideProps = async () => ({
-  redirect: {
-    destination: "/projects",
-    permanent: false,
-  },
-});
