cbe348371da9646f0021b1a5b941bad518db3e9a
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-04-07 20:13:25 +0200

    Add round metadata to landing page

--- src/pages/[domain]/index.tsx
+++ src/pages/[domain]/index.tsx
@@ -5,9 +5,12 @@ import { Button } from "~/components/ui/Button";
 import Link from "next/link";
 import { useCurrentRound } from "~/features/rounds/hooks/useRound";
 import { RoundProgress } from "~/features/info/components/RoundProgress";
-import Image from "next/image";
-import { cn } from "~/utils/classNames";
 import { Banner } from "~/components/ui/Banner";
+import { CircleDollarSign, Globe } from "lucide-react";
+import { networkNames } from "~/config";
+import { createComponent } from "~/components/ui";
+import { tv } from "tailwind-variants";
+import { useRoundToken } from "~/features/distribute/hooks/useAlloPool";
 
 export default function RoundPage() {
   const round = useCurrentRound();
@@ -20,7 +23,7 @@ export default function RoundPage() {
   if (resultAt) steps.push({ label: "Tallying", date: resultAt });
   if (payoutAt) steps.push({ label: "Distribution", date: payoutAt });
 
-  const { bannerImageUrl } = round.data ?? {};
+  const { bannerImageUrl, network } = round.data ?? {};
   return (
     <Layout>
       <div className="mb-2 flex items-center justify-between">
@@ -33,13 +36,40 @@ export default function RoundPage() {
           Apply with your project
         </Button>
       </div>
+      <div className="mb-4 flex gap-16">
+        <Meta>
+          <MetaLabel>Network</MetaLabel>
+          <div>{network ? networkNames[network] : "not set"}</div>
+        </Meta>
+        <Meta>
+          <MetaLabel>Token</MetaLabel>
+          <TokenSymbol />
+        </Meta>
+        <Meta>
+          <MetaLabel>Calculation</MetaLabel>
+          {round.data?.calculationType?.toUpperCase()}
+        </Meta>
+      </div>
       <Banner size="lg" src={bannerImageUrl} />
       {round.data && <RoundProgress steps={steps} />}
       <Markdown>{round.data?.description}</Markdown>
     </Layout>
   );
 }
+function TokenSymbol() {
+  const token = useRoundToken();
+  return <>{token.data?.symbol}</>;
+}
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   const { domain } = ctx.query;
   return { props: { domain } };
 };
+
+const Meta = createComponent(
+  "div",
+  tv({ base: "flex flex-col text-gray-600 font-semibold text-xl" }),
+);
+const MetaLabel = createComponent(
+  "div",
+  tv({ base: "uppercase text-sm font-normal" }),
+);
