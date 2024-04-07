ae20d3586cfab6d7fc202e6f0460120c9f529b7a
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-04-07 18:05:00 +0200

    Add landing page content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Plus } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "~/components/ui/Button";
+import { Markdown } from "~/components/ui/Markdown";
 import { RoundProgress } from "~/features/info/components/RoundProgress";
 import { Layout } from "~/layouts/DefaultLayout";
 
@@ -16,11 +17,23 @@ export default function LandingPage({}) {
       </div>
       <RoundProgress />
       <Image
+        className="mb-4"
         alt="Filecoin RetroPGF"
         src={"/filrpgf.jpg"}
         width={1600}
         height={900}
       />
+      <Markdown>
+        {`###  Welcome to FIL-RetroPGF-1, the Filecoin network’s first Optimism-inspired Retroactive Public Goods Funding round.
+
+The round will allocate FIL tokens to projects that have created an impact to the Filecoin network between Oct 2023 - Mar 2024. Future impact windows will be rewarded in future rounds.
+
+Between Apr 8th and Apr 21st, eligible nominations may submit an application. Projects that are eligible are listed <link to notion ‘whitelist’>. 
+
+Badgeholders will then vote on the projects between Apr 22nd and May 6th.
+
+For more information, visit [FIL-RetroPGF documentation](https://fil-retropgf.notion.site/FIL-RetroPGF-4b6f5358440043c8bb1bf53f0297541e).`}
+      </Markdown>
     </Layout>
   );
 }
