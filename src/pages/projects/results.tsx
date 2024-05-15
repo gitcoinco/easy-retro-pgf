70a1dcd68b050507af8e251186bda9e9cae83de1
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-05-15 17:35:18 +0200

    Results page

--- src/pages/projects/results.tsx
+++ src/pages/projects/results.tsx
@@ -2,10 +2,5 @@ import { Layout } from "~/layouts/DefaultLayout";
 import { ProjectsResults } from "~/features/projects/components/ProjectsResults";
 
 export default function ProjectsResultsPage() {
-  return null;
-  return (
-    <Layout>
-      <ProjectsResults />
-    </Layout>
-  );
+  return <Layout>{/* <ProjectsResults /> */}</Layout>;
 }
