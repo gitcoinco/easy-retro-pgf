15f81b72042f4e8341c8cc7d0645214be6ad7a3c
Crisgarner <@crisgarner>
2024-05-15 12:02:22 -0600

    fix: remove unused class in heading

--- src/features/admin/components/InvalidAdmin.tsx
+++ src/features/admin/components/InvalidAdmin.tsx
@@ -3,7 +3,7 @@ import { Heading } from "~/components/ui/Heading";
 export function InvalidAdmin() {
   return (
     <div>
-      <Heading className="truncate" size="2xl" as="h3">
+      <Heading size="2xl" as="h3">
         Invalid Admin Account
       </Heading>
       <p>Only admins can access this page.</p>
