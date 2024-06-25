7e70ff81f16c68efeed59afd8ddac3ce4e8f6c7a
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-06-25 11:44:31 +0200

    Add API keys router

---
+++ src/server/api/routers/apiKeys.ts
@@ -0,0 +1,39 @@
+import { createHash } from "crypto";
+import { z } from "zod";
+import { adminProcedure, createTRPCRouter } from "~/server/api/trpc";
+
+export function hashApiKey(apiKey: string) {
+  return createHash("sha256").update(apiKey).digest("hex");
+}
+
+const genApiKey = () => hashApiKey(crypto.randomUUID());
+
+export const apiKeysRouter = createTRPCRouter({
+  get: adminProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input: { id } }) => {
+      const creatorId = ctx.session?.user.name!;
+      return ctx.db.apiKey.findFirst({ where: { id, creatorId } });
+    }),
+  list: adminProcedure.query(async ({ ctx }) => {
+    const creatorId = ctx.session?.user.name!;
+    return ctx.db.apiKey.findMany({ where: { creatorId } });
+  }),
+  delete: adminProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input: { id } }) => {
+      const creatorId = ctx.session?.user.name!;
+      return ctx.db.apiKey.delete({ where: { id, creatorId } });
+    }),
+  create: adminProcedure.mutation(async ({ ctx }) => {
+    const key = genApiKey();
+
+    const creatorId = ctx.session?.user.name!;
+
+    await ctx.db.apiKey.create({
+      data: { creatorId, key: hashApiKey(key) },
+    });
+
+    return { key };
+  }),
+});
