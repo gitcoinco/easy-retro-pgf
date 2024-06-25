bf54f81b21cbab7b86409fcc5c9f19f61aeda9fa
Hussein Martinez <husse.dev@gmail.com>
2024-06-25 16:35:04 +0700

    chore: refactored Markdown component to include metrics type style as per RF4

--- src/components/ui/Markdown.tsx
+++ src/components/ui/Markdown.tsx
@@ -2,21 +2,34 @@ import clsx from "clsx";
 import { type ComponentProps } from "react";
 import ReactMarkdown from "react-markdown";
 import { Link } from "./Link";
+import { tv } from "tailwind-variants";
+
+const markdownVariants = tv({
+  base: "prose",
+  variants: {
+    type: {
+      standard: "max-w-none dark:prose-invert",
+      metrics: "line-clamp-2 text-gray-700",
+    },
+  },
+  defaultVariants: {
+    type: "standard",
+  },
+});
 
 const components = {
   a: (p: ComponentProps<typeof Link>) => <Link target="_blank" {...p} />,
 };
 export function Markdown({
   isLoading,
+  type,
   ...props
-}: { isLoading?: boolean } & ComponentProps<typeof ReactMarkdown>) {
+}: { isLoading?: boolean; type?: "metrics" } & ComponentProps<
+  typeof ReactMarkdown
+>) {
+  const className = markdownVariants({ type });
   return (
-    <div
-      className={clsx("prose max-w-none dark:prose-invert", {
-        ["h-96 animate-pulse rounded-xl bg-gray-100 dark:bg-gray-800"]:
-          isLoading,
-      })}
-    >
+    <div className={className}>
       <ReactMarkdown components={components} {...props} />
     </div>
   );
