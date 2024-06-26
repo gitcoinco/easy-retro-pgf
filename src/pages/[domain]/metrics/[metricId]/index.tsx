26b4b9236e92ab3813d67d9c49c1f42e79d1bd4b
Hussein Martinez <husse.dev@gmail.com>
2024-06-26 21:00:37 +0700

    refactor: MetricsDetailPage to render MetricDetail and MetricsSidebar

--- src/pages/[domain]/metrics/[metricId]/index.tsx
+++ src/pages/[domain]/metrics/[metricId]/index.tsx
@@ -1,13 +1,30 @@
 import { GetServerSideProps } from "next";
-import { api } from "~/utils/api";
+import { MetricsSidebar } from "~/features/metrics/components/MetricsSidebar";
+import MetricDetails from "~/features/metrics/components/MetricDetails";
+import { MetricsLayout } from "~/layouts/MetricsLayout";
+import { useMetricById } from "~/hooks/useMetrics";
 
 export default function MetricsDetailPage({ metricId = "" }) {
-  const { data: projects } = api.metrics.forProjects.useQuery({ metricId });
+  const { data, isPending } = useMetricById(metricId);
+
+  const { name = "", description = "", projects = [] } = data ?? {};
   return (
-    <div>
-      <p>Metric: {metricId}</p>
-      <pre>{JSON.stringify(projects, null, 2)}</pre>
-    </div>
+    <MetricsLayout>
+      <div className="mx-auto mb-24 flex max-w-screen-lg gap-8 px-4 pb-32 pt-16">
+        {isPending ? (
+          <div> Loading...</div>
+        ) : (
+          <>
+            <section className="flex-col">
+              <MetricDetails name={name} description={description} />
+            </section>
+            <aside>
+              <MetricsSidebar projects={projects} />
+            </aside>
+          </>
+        )}
+      </div>
+    </MetricsLayout>
   );
 }
 
