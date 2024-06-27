82ca050e63f205c0684efb3c6046cb4a86965868
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-06-27 09:11:22 +0200

    Add CORS

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,18 +1,32 @@
-import { createNextApiHandler } from "@trpc/server/adapters/next";
+import {
+  NextApiRequest,
+  NextApiResponse,
+  createNextApiHandler,
+} from "@trpc/server/adapters/next";
 
 import { env } from "~/env";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
-export default createNextApiHandler({
+const nextApiHandler = createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
-  onError:
-    env.NODE_ENV === "development"
-      ? ({ path, error }) => {
-          console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
-          );
-        }
-      : undefined,
 });
+// @link https://nextjs.org/docs/api-routes/introduction
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Request-Method", "*");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Referrer-Policy", "no-referrer");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    res.writeHead(200);
+    return res.end();
+  }
+  // finally pass the request on to the tRPC handler
+  return nextApiHandler(req, res);
+}
