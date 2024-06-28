4e0c5b1379018f1591acc5c827610b66d18b9d0a
Hussein Martinez <husse.dev@gmail.com>
2024-06-28 22:55:11 +0700

    refactor: MetricsSidebar is more generic now to be included in MetricsLayout

--- src/features/metrics/components/MetricsSidebar/MetricsSidebar.tsx
+++ src/features/metrics/components/MetricsSidebar/MetricsSidebar.tsx
@@ -1,78 +1,16 @@
-"use client";
-import { useMemo, useState } from "react";
+import React from "react";
+import { SidebarMetric } from "./SidebarMetric";
+import { useCurrentDomain } from "~/features/rounds/hooks/useRound";
+import { useCurrentMetricId } from "~/features/metrics/hooks/useMetrics";
+import { SidebarPlaceholder } from "./SidebarPlaceholder";
 
-import { Card } from "~/components/ui/Card";
-import { Heading } from "~/components/ui/Heading";
-import { ScrollArea } from "~/components/ui/ScrollArea";
-import { Button } from "../Buttons";
-import { AllocationList } from "./AllocationList";
-import { MetricProject } from "~/utils/fetchMetrics";
-import { CustomLineChart } from "../Charts";
-import { ArrowDownNarrowWide, ArrowUpWideNarrow } from "lucide-react";
+export function MetricsSidebar() {
+  const domain = useCurrentDomain();
+  const metricId = useCurrentMetricId();
 
-type MetricsSidebarProps = {
-  projects: MetricProject[];
-};
+  if (metricId) {
+    return <SidebarMetric metricId={metricId} />;
+  }
 
-const parseProjectsDataToChartData = (projects: MetricProject[]) => {
-  return projects.map((project, index) => ({ x: index, y: project.fraction }));
-};
-
-const sortProjectsData = (
-  projects: MetricProject[],
-  sortBy: "ascending" | "descending",
-) => {
-  return projects.sort((a, b) =>
-    sortBy === "descending" ? b.fraction - a.fraction : a.fraction - b.fraction,
-  );
-};
-
-const title = "Distribution";
-const description = "For this particular metric";
-
-export function MetricsSidebar({ projects }: MetricsSidebarProps) {
-  const [sortOrder, setSortOrder] = useState<"ascending" | "descending">(
-    "descending",
-  );
-
-  const toggleSortOrder = () =>
-    setSortOrder(sortOrder === "ascending" ? "descending" : "ascending");
-
-  const { sortedProjects, chartData } = useMemo(() => {
-    const sortedProjects = sortProjectsData(projects, sortOrder);
-    const chartData = parseProjectsDataToChartData(sortedProjects);
-    return { sortedProjects, chartData };
-  }, [projects, sortOrder]);
-
-  return (
-    <Card className="sticky top-4 w-[300px]">
-      <div className="p-3">
-        <Heading size="xl">{title}</Heading>
-        {description && <p className="mb-4 leading-relaxed">{description}</p>}
-      </div>
-      <div className="space-y-2 p-3">
-        <div className="space-y-1">
-          <div className="h-32 rounded-lg border">
-            <CustomLineChart data={chartData} />
-          </div>
-          <div className="flex justify-end gap-1">
-            <Button
-              size="xs"
-              iconRight={
-                sortOrder === "ascending"
-                  ? ArrowUpWideNarrow
-                  : ArrowDownNarrowWide
-              }
-              onClick={toggleSortOrder}
-            >
-              {sortOrder === "ascending" ? "Ascending" : "Descending"}
-            </Button>
-          </div>
-        </div>
-        <ScrollArea className="relative max-h-[328px]">
-          <AllocationList projects={sortedProjects} />
-        </ScrollArea>
-      </div>
-    </Card>
-  );
+  return <SidebarPlaceholder title="">No metric id</SidebarPlaceholder>;
 }
