bd9af06b0a79b4613e4912cc5fb2d74d85129680
Hussein Martinez <husse.dev@gmail.com>
2024-06-28 22:56:50 +0700

    chore: render MetricsSidebar in MetricsLayout

--- src/layouts/MetricsLayout.tsx
+++ src/layouts/MetricsLayout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode, PropsWithChildren } from "react";
 import Header from "~/components/Header";
 import { BaseLayout, type LayoutProps } from "./BaseLayout";
 import { useCurrentDomain } from "~/features/rounds/hooks/useRound";
+import { MetricsSidebar } from "~/features/metrics/components/MetricsSidebar";
 
 type Props = PropsWithChildren<
   {
@@ -22,7 +23,12 @@ export const MetricsLayout = ({ children, ...props }: Props) => {
 
   return (
     <BaseLayout {...props} header={<Header navLinks={navLinks} />}>
-      {children}
+      <div className="mx-auto mb-24 flex max-w-screen-lg gap-8 px-4 pb-32 pt-16">
+        <section className="flex-1 space-y-6">{children}</section>
+        <aside>
+          <MetricsSidebar />
+        </aside>
+      </div>
     </BaseLayout>
   );
 };
--- src/pages/[domain]/metrics/[metricId]/index.tsx
+++ src/pages/[domain]/metrics/[metricId]/index.tsx
@@ -1,8 +1,7 @@
 import { GetServerSideProps } from "next";
-import { MetricsSidebar } from "~/features/metrics/components/MetricsSidebar";
 import MetricDetails from "~/features/metrics/components/MetricDetails";
 import { MetricsLayout } from "~/layouts/MetricsLayout";
-import { useMetricById } from "~/hooks/useMetrics";
+import { useMetricById } from "~/features/metrics/hooks/useMetrics";
 import { Spinner } from "~/components/ui/Spinner";
 
 type MetricsDetailPageProps = {
@@ -18,30 +17,25 @@ export default function MetricsDetailPage({
 
   return (
     <MetricsLayout>
-      <div className="mx-auto mb-24 flex max-w-screen-lg gap-8 px-4 pb-32 pt-16">
-        {isPending ? (
+      {isPending ? (
+        <>
           <div className="flex h-full w-full items-center justify-center">
             <div className="flex flex-col items-center space-y-2">
               <Spinner className="mb-4 h-8 w-8" />
               <p className="text-gray-700">Loading metric details...</p>
             </div>
           </div>
-        ) : (
-          <>
-            <section className="flex-1 space-y-6">
-              <div className="mb-6 h-5">{"> BREADCRUMBS"}</div>
-              <MetricDetails name={name} description={description} />
-              <div className="space-y-4">{"COMMENTS"}</div>
-              <div className="fixed bottom-10 left-1/2 z-10 -translate-x-1/2 rounded-lg border bg-white p-2 shadow">
-                {"PAGINATION"}
-              </div>
-            </section>
-            <aside>
-              <MetricsSidebar projects={projects} />
-            </aside>
-          </>
-        )}
-      </div>
+        </>
+      ) : (
+        <>
+          <div className="mb-6 h-5">{"> BREADCRUMBS"}</div>
+          <MetricDetails name={name} description={description} />
+          <div className="space-y-4">{"COMMENTS"}</div>
+          <div className="fixed bottom-10 left-1/2 z-10 -translate-x-1/2 rounded-lg border bg-white p-2 shadow">
+            {"PAGINATION"}
+          </div>
+        </>
+      )}
     </MetricsLayout>
   );
 }
