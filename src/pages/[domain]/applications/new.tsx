f84e04217372b845074a344740267b53e31ffafe
Hussein Martinez <husse.dev@gmail.com>
2024-07-10 14:40:23 +0700

    fix: render alert if application period has finished
    - If application period has entered we want to render an informative alert instead of the application form

--- src/pages/[domain]/applications/new.tsx
+++ src/pages/[domain]/applications/new.tsx
@@ -1,36 +1,55 @@
+"use client";
+
 import { Layout } from "~/layouts/DefaultLayout";
 
 import { ApplicationForm } from "~/features/applications/components/ApplicationForm";
 import { useAccount } from "wagmi";
 import { Alert } from "~/components/ui/Alert";
 import { FormSection } from "~/components/ui/Form";
+import { useRoundState } from "~/features/rounds/hooks/useRoundState";
 
 export default function NewProjectPage() {
   const { address } = useAccount();
 
+  const roundState = useRoundState();
+  const isNotApplicationPeriod = roundState !== "APPLICATION";
+
   return (
     <Layout>
-      <FormSection
-        title="New application"
-        description={
-          <>
-            <p>
-              Fill out this form to create an application for your project. It
-              will then be reviewed by our admins.
-            </p>
-            <p>
-              Your progress is saved locally so you can return to this page to
-              resume your application.
-            </p>
-          </>
-        }
-      >
-        {address ? (
-          <ApplicationForm address={address} />
-        ) : (
-          <Alert variant="info" title="Connect your wallet to continue"></Alert>
-        )}
-      </FormSection>
+      {isNotApplicationPeriod ? (
+        <FormSection
+          title="New application"
+          description={
+            <Alert
+              className="mt-4"
+              variant="info"
+              title="Application period has ended"
+            />
+          }
+        ></FormSection>
+      ) : (
+        <FormSection
+          title="New application"
+          description={
+            <>
+              <p>
+                Fill out this form to create an application for your project. It
+                will then be reviewed by our admins.
+              </p>
+              <p>
+                Your progress is saved locally so you can return to this page to
+                resume your application.
+              </p>
+            </>
+          }
+        >
+          {address ? (
+            <ApplicationForm address={address} />
+          ) : (
+            <Alert variant="info" title="Connect your wallet to continue" />
+          )}
+        </FormSection>
+      )}
     </Layout>
   );
 }
