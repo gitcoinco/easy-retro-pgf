f6695a9f1a636648dcedb4078e9a3e67a5d9931e
Huss Martinez <husse.dev@gmail.com>
2024-08-22 04:16:51 +0700

    fix: reintroduce projects endpoint in metricsRouter (#105)

--- src/server/api/routers/metrics/metricsRouter.ts
+++ src/server/api/routers/metrics/metricsRouter.ts
@@ -12,6 +12,7 @@ import { getMetrics } from "./getMetrics";
 import { fetchMetricsForRound } from "./fetchMetricsForRound";
 import { fetchMetricsForBallot } from "./fetchMetricsForBallots";
 import { fetchMetricsForProjects } from "./fetchMetricsForProjects";
+import { getProjects } from "./getProjects";
 
 export const metricsRouter = createTRPCRouter({
   get: publicProcedure
@@ -67,6 +68,20 @@ export const metricsRouter = createTRPCRouter({
           metricIds,
           roundId,
         });
+      } catch (error) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: (error as Error).message,
+        });
+      }
+    }),
+
+  projects: roundProcedure
+    .input(z.object({ projectIds: z.array(z.string()) }))
+    .query(async ({ input: { projectIds }, ctx }) => {
+      try {
+        const { metrics: metricIds } = ctx.round;
+        return getProjects({ projectIds, metricIds });
       } catch (error) {
         throw new TRPCError({
           code: "BAD_REQUEST",
