b2c25dd6dc8c9f2cade8d328931a415f97ff487a
Hussein Martinez <husse.dev@gmail.com>
2024-08-21 11:40:45 +0700

    chore: removed api.metrics.projects
    reason: unused and using legacy mocking for projects

--- src/server/api/routers/metrics/metricsRouter.ts
+++ src/server/api/routers/metrics/metricsRouter.ts
@@ -11,7 +11,6 @@ import { getMetrics } from "./getMetrics";
 import { fetchMetricsForRound } from "./fetchMetricsForRound";
 import { fetchMetricsForBallot } from "./fetchMetricsForBallots";
 import { fetchMetricsForProjects } from "./fetchMetricsForProjects";
-import { getProjects } from "./getProjects";
 
 export const metricsRouter = createTRPCRouter({
   get: publicProcedure
@@ -49,19 +48,6 @@ export const metricsRouter = createTRPCRouter({
     .query(async ({ input: { metricIds }, ctx }) => {
       try {
         return fetchMetricsForProjects({ metricIds });
-      } catch (error) {
-        throw new TRPCError({
-          code: "BAD_REQUEST",
-          message: (error as Error).message,
-        });
-      }
-    }),
-  projects: roundProcedure
-    .input(z.object({ projectIds: z.array(z.string()) }))
-    .query(async ({ input: { projectIds }, ctx }) => {
-      try {
-        const { metrics: metricIds } = ctx.round;
-        return getProjects({ projectIds, metricIds });
       } catch (error) {
         throw new TRPCError({
           code: "BAD_REQUEST",
