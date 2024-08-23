32bc6a17145a89ed4b62c1097188d0b6f3566995
Nick Lionis <nick-w3@Nicks-MacBook-Pro.local>
2024-08-23 12:14:23 +0300

    fixed error handling refactored using wagmi instead of eas-sdk

---
+++ src/utils/errorHandler.ts
@@ -0,0 +1,50 @@
+import {
+  BaseError,
+  ContractFunctionRevertedError,
+  InsufficientFundsError,
+  UserRejectedRequestError,
+} from "viem";
+
+export function handleTransactionError(error: any) {
+  console.log("error", error);
+  if (error instanceof BaseError) {
+    const revertError = error.walk(
+      (err) => err instanceof ContractFunctionRevertedError,
+    );
+    if (revertError instanceof ContractFunctionRevertedError) {
+      const errorName = revertError.data?.errorName ?? "Unknown Error";
+      console.log("errorName", errorName);
+      throw { reason: errorName, data: { message: errorName } };
+    }
+
+    const isInsufficientFundsError =
+      error.walk((e) => e instanceof InsufficientFundsError) instanceof
+      InsufficientFundsError;
+    if (isInsufficientFundsError) {
+      throw {
+        reason: "Insufficient Funds",
+        data: { message: "Insufficient Funds" },
+      };
+    }
+    const isUserRejectedRequestError =
+      error.walk((e) => e instanceof UserRejectedRequestError) instanceof
+      UserRejectedRequestError;
+    if (isUserRejectedRequestError) {
+      throw {
+        reason: "User Rejected Request",
+        data: { message: "User Rejected Request" },
+      };
+    }
+  }
+
+  throw {
+    reason: "Transaction Failed",
+    data: { message: String("unknown reason") },
+  };
+}
+
+export function getIsFilecoinActorError(error: string) {
+  return JSON.stringify(error).includes(
+    "call raw get actor: resolution lookup failed",
+  );
+}
