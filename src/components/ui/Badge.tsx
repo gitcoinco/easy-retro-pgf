2b219764940e13bb56e57fc42930b4a294eee44a
Hussein Martinez <husse.dev@gmail.com>
2024-08-27 14:36:23 +0700

    feat: added info variant and xs size to Badge component

--- src/components/ui/Badge.tsx
+++ src/components/ui/Badge.tsx
@@ -8,9 +8,11 @@ export const Badge = createComponent(
     variants: {
       variant: {
         default: "bg-gray-100 dark:bg-gray-800",
+        info: "bg-blue-100 dark:bg-blue-300 text-blue-900",
         success: "bg-green-100 dark:bg-green-300 text-green-900",
       },
       size: {
+        xs: "px-1 text-xs",
         md: "px-1",
         lg: "px-2 py-1 text-base",
       },
