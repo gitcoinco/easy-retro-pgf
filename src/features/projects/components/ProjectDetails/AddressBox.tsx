4dfb885c4d377da640312fa2e28b8fc8322ce74f
Hussein Martinez <husse.dev@gmail.com>
2024-08-27 18:34:37 +0700

    chore: implemented AddressBox

---
+++ src/features/projects/components/ProjectDetails/AddressBox.tsx
@@ -0,0 +1,16 @@
+export function AddressBox({
+  label,
+  addresses,
+}: {
+  label: string;
+  addresses?: string[];
+}) {
+  return (
+    <div className="rounded-xl border p-3 dark:border-gray-700">
+      <div className="mb-2 font-bold tracking-wider text-gray-600 dark:text-gray-500">
+        {label}
+      </div>
+      <div className="space-y-2">{addresses}</div>
+    </div>
+  );
+}
