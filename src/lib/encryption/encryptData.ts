cede7a73f261a950ffa1c2dfc254a3b9d569ce31
eagle <aparokshambhargav@gmail.com>
2024-08-28 22:55:01 +0530

    feat: update encryptData functions

--- src/lib/encryption/encryptData.ts
+++ src/lib/encryption/encryptData.ts
@@ -1,13 +1,12 @@
 import crypto from "crypto";
 
 const algorithm = "aes-256-cbc";
-const key = process.env.ENCRYPTION_KEY;
+const key = process.env.NEXT_PUBLIC_ENCRYPTION_KEY;
 const iv = crypto.randomBytes(16); // generate a random initialization vector
 
 if (!key) {
   throw new Error("ENCRYPTION_KEY is not set");
 }
-
 interface EncryptedData {
   iv: string;
   data: string;
@@ -25,13 +24,17 @@ export const encryptData = (data: object): EncryptedData => {
 
 export const decryptData = (encryptedData: EncryptedData): object => {
   const iv = Buffer.from(encryptedData.iv, "hex");
-  const encryptedText = Buffer.from(encryptedData.data, "hex");
+  const encryptedText = encryptedData.data;
   const decipher = crypto.createDecipheriv(
     algorithm,
     Buffer.from(key, "hex"),
     iv,
   );
-  let decrypted = decipher.update(encryptedText.toString(), "hex", "utf8");
+  let decrypted = decipher.update(encryptedText, "hex", "utf8");
   decrypted += decipher.final("utf8");
   return JSON.parse(decrypted) as object;
+};
+
+export const generateKey = () => {
+  return crypto.randomBytes(32).toString("hex");
 };
