03aecc2e02bdfe44609178e7708f96a2dc73c5ab
Hussein Martinez <husse.dev@gmail.com>
2024-08-30 00:58:04 +0700

    refactor: useIsCorrectNetwork and EnsureCorrectNetwork
    useIsCorrectNetwork:
    
    - added force change network functionality
    - now returns switchChain function from wagmi
    
    EnsureCorrectNetwork:
    
    - use switchChain from useIsCorrectNetwork

--- src/hooks/useIsCorrectNetwork.ts
+++ src/hooks/useIsCorrectNetwork.ts
@@ -2,10 +2,13 @@ import { supportedNetworks } from "~/config";
 import { useCurrentRound } from "~/features/rounds/hooks/useRound";
 import { useAccount, useChainId } from "wagmi";
 import { useSwitchChain } from "wagmi";
+import { useEffect } from "react";
 
 export function useIsCorrectNetwork(
   opts: { force: boolean } = { force: false },
 ) {
+  const forceChange = opts.force;
+
   const { isConnected } = useAccount();
   const chainId = useChainId();
   const { switchChain } = useSwitchChain();
@@ -15,9 +18,11 @@ export function useIsCorrectNetwork(
   const network = supportedNetworks.find((n) => n.chain === round?.network);
   const isCorrectNetwork = isConnected && chainId === network?.id;
 
-  if (opts.force && isConnected && !isCorrectNetwork) {
-    switchChain({ chainId: network?.id as number });
-  }
+  useEffect(() => {
+    if (forceChange && isConnected && !isCorrectNetwork) {
+      switchChain({ chainId: network?.id as number });
+    }
+  }, [forceChange, isConnected, isCorrectNetwork, network]);
 
   return {
     switchChain,
