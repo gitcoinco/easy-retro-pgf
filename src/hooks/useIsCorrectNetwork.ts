75d3438c996801b74237f1a53f8b7bda4ab294eb
Hussein Martinez <husse.dev@gmail.com>
2024-08-30 00:55:30 +0700

    feat: extended useIsCorrectNetwork adding a force change flag and returning switchChain function

--- src/hooks/useIsCorrectNetwork.ts
+++ src/hooks/useIsCorrectNetwork.ts
@@ -1,17 +1,26 @@
 import { supportedNetworks } from "~/config";
 import { useCurrentRound } from "~/features/rounds/hooks/useRound";
 import { useAccount, useChainId } from "wagmi";
+import { useSwitchChain } from "wagmi";
 
-export function useIsCorrectNetwork() {
+export function useIsCorrectNetwork(
+  opts: { force: boolean } = { force: false },
+) {
   const { isConnected } = useAccount();
   const chainId = useChainId();
+  const { switchChain } = useSwitchChain();
 
   const { data: round } = useCurrentRound();
 
   const network = supportedNetworks.find((n) => n.chain === round?.network);
   const isCorrectNetwork = isConnected && chainId === network?.id;
 
+  if (opts.force && isConnected && !isCorrectNetwork) {
+    switchChain({ chainId: network?.id as number });
+  }
+
   return {
+    switchChain,
     isCorrectNetwork,
     correctNetwork: network,
   };
