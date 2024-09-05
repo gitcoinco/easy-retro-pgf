ab3c62595f71d92c17b6a8191ef1c443eaec7b32
Hussein Martinez <husse.dev@gmail.com>
2024-09-06 03:31:28 +0700

    chore: implemented api.projects.listSunnyProjects

--- src/server/api/routers/projects.ts
+++ src/server/api/routers/projects.ts
@@ -1,18 +1,24 @@
 import { z } from "zod";
 
-import { attestationProcedure, createTRPCRouter } from "~/server/api/trpc";
 import {
-  Attestation,
+  attestationProcedure,
+  createTRPCRouter,
+  roundProcedure,
+} from "~/server/api/trpc";
+import {
+  type Attestation,
+  createAttestationFetcher,
   createDataFilter,
   createSearchFilter,
 } from "~/utils/fetchAttestations";
 import { TRPCError } from "@trpc/server";
-import { eas } from "~/config";
 import { type Filter, FilterSchema } from "~/features/filter/types";
 import { fetchMetadata } from "~/utils/fetchMetadata";
 import { fetchProfiles } from "./profile/utils";
 import { fetchImpactMetricsFromCSV } from "~/utils/fetchMetrics";
-import { OSOMetricsCSV } from "~/types";
+import { getApplicationStatus } from "./applications/utils";
+import type { OSOMetric, OSOMetricsCSV } from "~/types";
+import type { ApplicationStatus } from "./applications/types";
 
 export const projectsRouter = createTRPCRouter({
   count: attestationProcedure.query(async ({ ctx }) => {
@@ -243,6 +249,118 @@ export const projectsRouter = createTRPCRouter({
       }
     }),
 
+  listSunnyProjects: roundProcedure
+    .input(FilterSchema)
+    .query(
+      async ({
+        input: {
+          cursor: paginationCursor,
+          limit: paginationLimit,
+          orderBy,
+          sortOrder,
+        },
+        ctx: { round },
+      }) => {
+        if (!round)
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Round not found",
+          });
+        try {
+          const attestationFetcher = createAttestationFetcher({ round });
+
+          // Fetch Project applications
+          const projects = await attestationFetcher(
+            ["metadata"],
+            {
+              where: {
+                AND: [
+                  createDataFilter("type", "bytes32", "application"),
+                  createDataFilter("round", "bytes32", round.id),
+                ],
+              },
+              take: paginationLimit,
+              skip: paginationCursor * paginationLimit,
+              orderBy: [createOrderBy(orderBy, sortOrder)],
+            },
+            ["id", "decodedDataJson", "recipient"],
+          );
+
+          const statusByProjectId: Record<string, ApplicationStatus> = {};
+
+          // Fetch all status in parallel
+          await Promise.all(
+            projects.map(async ({ id: projectId }) => {
+              const { status } = await getApplicationStatus({
+                round,
+                projectId,
+              });
+
+              statusByProjectId[projectId] = status;
+            }),
+          );
+
+          const metadataByProjectId = await fetchMetadataForSunnys(projects);
+
+          // Include mock of 10 metrics
+          const metricsArray = await fetchImpactMetricsFromCSV({
+            projectIds: [
+              ...Object.keys(projects),
+              ...[
+                "id0",
+                "id1",
+                "id2",
+                "id3",
+                "id4",
+                "id5",
+                "id6",
+                "id7",
+                "id8",
+                "id9",
+              ],
+            ],
+          });
+
+          const metricsByProjectId: Record<
+            string,
+            Partial<OSOMetricsCSV>
+          > = Object.fromEntries(
+            metricsArray.map((metricsItem: OSOMetricsCSV) => {
+              const { project_id, project_name, ...metrics } = metricsItem;
+              return [project_id, metrics];
+            }),
+          );
+
+          const projectsResult: Array<
+            Attestation & {
+              status: ApplicationStatus;
+              metrics?: Partial<OSOMetricsCSV>;
+              metadata: unknown;
+            }
+          > = projects.map((project, index) => {
+            const { id: projectId } = project;
+            const status = statusByProjectId[projectId]!;
+            const metrics =
+              metricsByProjectId[projectId] ?? metricsByProjectId[`id${index}`];
+            const metadata = metadataByProjectId[projectId];
+
+            return {
+              ...project,
+              metadata,
+              metrics,
+              status,
+            };
+          });
+          return projectsResult;
+        } catch (error) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: (error as Error).message,
+          });
+        }
+      },
+    ),
+
   // Used for distribution to get the projects' payoutAddress
   // To get this data we need to fetch all projects and their metadata
   payoutAddresses: attestationProcedure
@@ -320,4 +438,18 @@ async function fetchMetadataForProjects(
       profiles: Record<string, unknown>;
     },
   );
+}
+
+async function fetchMetadataForSunnys(projects: Attestation[]) {
+  const metadataByProjectId: Record<string, unknown> = {};
+
+  await Promise.all(
+    projects.map(async ({ metadataPtr, id }) => {
+      if (!metadataPtr) return;
+      const metadata = await fetchMetadata(metadataPtr);
+      metadataByProjectId[id] = metadata;
+    }),
+  );
+
+  return metadataByProjectId;
 }
