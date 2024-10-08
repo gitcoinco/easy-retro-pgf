c7d15b48841175d8f1d2014e6422f6d1081e8983
HananINouman <hanan@obol.tech>
2024-10-08 20:51:37 +0300

    fix primary shades

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,16 @@ export default {
   theme: {
     extend: {
       colors: {
-        ...colors,
-        primary:"#2fe4ab",
+        primary: {
+          ...colors,  
+          100: "#2fe4ab",//Check with Leonardo
+          200: "#2fe4ab",
+          300: "#2fe4ab",
+          400: "#2fe4ab",
+          500: "#2fe4ab",
+          600: "#2fe4ab",
+          700: "#2fe4ab",
+        },
         gray: colors.stone,
       },
       fontFamily: {
