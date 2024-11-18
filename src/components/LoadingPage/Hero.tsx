4114eef4ecae6cddfbf07102e0423d31a215b61f
HananINouman <hanan@obol.tech>
2024-11-18 14:09:23 +0300

    fix responsiveness

--- src/components/LoadingPage/Hero.tsx
+++ src/components/LoadingPage/Hero.tsx
@@ -6,9 +6,9 @@ export const Hero = () => {
             <div className="mb-10 md:mb-0" >
                 <img alt="Background" src="/Hero.png" />
             </div>
-            <div className="md:absolute md:top-36 flex flex-col gap-10 items-center text-center">
-                <h1 className="sm:max-w-[332px] md:max-w-[625.61px] text-[#091011] text-[40px] md:text-[64px] leading-[40px] md:leading-[64px] font-medium">Obol Retroactive Funding</h1>
-                <p className="text-[32px] leading-[48px]">for the Obol Collective</p>
+            <div className="md:absolute flex flex-col items-center text-center md:bottom-[20%] gap-10  md:gap-2 lg:gap-4 xl:gap-10">
+                <h1 className="w-3/4 text-[#091011] text-4xl md:text-3xl lg:text-5xl xl:text-6xl  leading-10 lg:leading-normal font-medium">Obol Retroactive Funding</h1>
+                <p className="text-[32px] lg:text-3xl md:text-2xl xl:text-[32px] leading-tight">for the Obol Collective</p>
                 <ConnectButtonSection />
                 <a href="#what-is-raf" className="text-black text-lg font-bold leading-7">What is RAF?</a>
             </div>
