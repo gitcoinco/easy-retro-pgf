5c480b91d88dbb7d211d8cde23ceb23d66d5a9db
obol-platform <139374357+obol-platform@users.noreply.github.com>
2025-01-23 15:05:55 +0000

    Update ConnectButtonSection.tsx

--- src/components/LoadingPage/ConnectButtonSection.tsx
+++ src/components/LoadingPage/ConnectButtonSection.tsx
@@ -6,7 +6,7 @@ export const ConnectButtonSection = () =>
     <ConnectButton>
         {/* move it as a variant to button ui same as connect button */}
         {/* [TODO Hanan] This link will be changed with each round during testing phases*/}
-        <Button className="text-secondary-600 h-10 px-6 py-3 w-36 inline-flex items-center justify-center text-center transition-colors  backdrop-blur-sm  rounded-lg duration-150 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2" variant="primary" as={Link} href={"/obol-raf-1"}> 
+        <Button className="text-secondary-600 h-10 px-6 py-3 w-36 inline-flex items-center justify-center text-center transition-colors  backdrop-blur-sm  rounded-lg duration-150 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2" variant="primary" as={Link} href={"/raf1"}> 
             Go To App
         </Button>
     </ConnectButton>
